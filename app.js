a66a0f7d7157874b12ae0132df8cc0b0fe2a0861
Harris Pyo <HPW9229@gmail.com>
2023-06-12 16:07:06 -0400

    Added local storage saving to todo lists

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let todos;
 let categories;
 let noActive = true;
-localStorage.clear();
 
 function findCurrentCategory() {
   const category = document.getElementById('active-list').innerText;
@@ -157,6 +156,7 @@ function addCategory(newList) {
       renderCategories();
     }
     saveCategory();
+    saveTodos();
   }
 }
 
@@ -245,4 +245,4 @@ function swapLists(event) {
   renderTasks(categoryToSwitchTo);
 }
 firstCatagoriesRender();
-renderTasks('Youtube');
+renderTasks(findCurrentCategory());
